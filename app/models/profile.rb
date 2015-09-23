6bd2c90b486125e95aceeeb6ad839770a4a56570
Thomas Chu <itsthomaschu@gmail.com>
2015-09-23 07:57:13 +0000

    Finished community page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
     belongs_to :users
 
-    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://s3.amazonaws.com/uifaces/faces/twitter/bermonpainter/128.jpg"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
