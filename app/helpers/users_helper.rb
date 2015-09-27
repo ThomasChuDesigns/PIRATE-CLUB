d1aa0fae3c2bdf37247652246b0a8783fb694fe0
Thomas Chu <itsthomaschu@gmail.com>
2015-09-27 05:57:05 +0000

    improvements and finished posts implementation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
           "<h1 class='label label-danger'>Admin</h1>".html_safe
         elsif @user.rank_id == 2
           "<h1 class='label label-danger'>CAPTAIN</h1>".html_safe
-        elsif @user.rank_id == 3
+        else
           "<h1 class='label label-primary'>CREW MEMBER</h1>".html_safe
         end    
     end
